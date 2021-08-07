9f88872ae6398d426590a996078e44e8ae26bbf1
George Garcia <jorge.90gap@gmail.com>
2021-08-07 13:39:14 +0900

    User model created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/new'
-  get 'posts/edit'
+  
   resources :posts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_07_024858) do
+ActiveRecord::Schema.define(version: 2021_08_07_043721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 2021_08_07_024858) do
     t.string "title"
     t.text "content"
     t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.integer "password_disgest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
