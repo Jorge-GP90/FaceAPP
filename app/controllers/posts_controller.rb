201d343b9e2ba3df2bd80e1f767f85b14c47db99
George Garcia <jorge.90gap@gmail.com>
2021-08-07 13:54:13 +0900

    Post controller settings

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,10 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
+    @post = Post.new
   end
 
   def edit
@@ -19,4 +21,10 @@ class PostsController < ApplicationController
   
   def destroy
   end
+
+  Private
+  def post_params
+    params.require(:post).permit(:title, :content, :image, :image_cache)
+  end
+
 end
