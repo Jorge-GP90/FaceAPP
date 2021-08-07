ca5dd43607a19f49472b50a34f6d742bd35cb55e
George Garcia <jorge.90gap@gmail.com>
2021-08-07 14:03:44 +0900

    user controller settings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 class UsersController < ApplicationController
+  before_action :set_user, :only [:show, :edit, :update, :destroy]
   def index
+    @users = User.all
   end
 
   def new
+    @user = User.new
   end
 
   def edit
@@ -12,11 +15,21 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
   end
 
   def update
   end
 
   def destroy
+  end
+
+  Private
+  def user_params
+    params.require(:user).permit(:name, :email, :password_digest)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
