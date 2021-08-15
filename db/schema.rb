3d1954b4d23edff858b036ca7b6586a37dab453d
George Garcia <jorge.90gap@gmail.com>
2021-08-15 21:08:47 +0900

    Modified users column name: password digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_07_043721) do
+ActiveRecord::Schema.define(version: 2021_08_15_120418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 2021_08_07_043721) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.integer "password_disgest"
+    t.integer "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
