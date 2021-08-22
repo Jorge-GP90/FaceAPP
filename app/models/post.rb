c96b8ed5682b109758005e32f0af1cd0b3df4ec1
George Garcia <jorge.90gap@gmail.com>
2021-08-22 19:50:15 +0900

    Image Update has setted up

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :content, length: { in: 1..250 }
   belongs_to :user
-  # mount_uploader :image, ImageUploader
+  mount_uploader :image, ImageUploader
 end
